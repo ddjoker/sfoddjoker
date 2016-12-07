297da110da6d1cad60cba004a31d67e1d9a61572
dong <1986jingdie@163.com>
2016-12-07 23:03:37 +0800

    双向链表模型

---
+++ src/main/java/model/link/LinkModel.java
@@ -0,0 +1,41 @@
+package model.link;
+
+/**
+ * 双向链表
+ * Created by dong on 2016/12/7.
+ */
+public class LinkModel<T> {
+    private LinkModel<T> previous;
+
+    private T t ;
+
+    private LinkModel<T> latter;
+
+    public void setPrevious(LinkModel<T> previous) {
+        this.previous = previous;
+    }
+
+    public void setLatter(LinkModel<T> latter) {
+        this.latter = latter;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+
+    public LinkModel<T> getLatter() {
+        return latter;
+    }
+
+    public LinkModel<T> getPrevious() {
+        return previous;
+    }
+
+    public T getT() {
+        return t;
+    }
+
+    public LinkModel(T t) {
+        this.t = t;
+    }
+}
